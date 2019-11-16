52eb71446e42f2770e745ec929fd0e4e0e6427e9
Barry Kaplan <barry@systeminsights.com>
2019-11-15 17:24:51 -0800

    APP-2241 remove tc:version and version.js

--- version.js
+++
@@ -1,2 +0,0 @@
-console.log("##teamcity[setParameter name='vi-version-number' value='"+process.env.npm_package_version+"']");
-console.log("##teamcity[buildNumber '"+ process.env.npm_package_version+"-{build.number}']");
