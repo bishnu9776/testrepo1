cb23b973160ad4acc2367b3ee7d11e032168f4ea
rosmirehman <rosmi@systeminsights.com>
2020-05-29 12:36:02 +0530

    DP-34 - updated channel info for pre big sink.

--- src/metrics/tags.js
+++ src/metrics/tags.js
@@ -1,9 +1,12 @@
 import {path} from "ramda"
 
 const debugStats = JSON.parse(process.env.VI_STATS_PER_DEVICE || "false")
+const isPreBigSinkInput = JSON.parse(process.env.VI_PRE_BIG_SINK_INPUT || "false")
 
 export const getGCPMessageTags = message => {
-  const channel = path(["attributes", "channel"], message)
+  const channel = isPreBigSinkInput
+    ? path(["attributes", "subFolder"], message)
+    : path(["attributes", "channel"], message)
   const device_uuid = path(["attributes", "bike_id"], message) // eslint-disable-line
   return debugStats
     ? {
