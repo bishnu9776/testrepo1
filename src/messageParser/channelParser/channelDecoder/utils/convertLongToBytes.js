be94bdd385c5347b489ad49d29d244936edf8743
siddhant <siddhant@systeminsights.com>
2020-06-04 09:52:24 +0530

    DP-34 added support for negative values (wip)

--- src/messageParser/channelParser/channelDecoder/utils/convertLongToBytes.js
+++ src/messageParser/channelParser/channelDecoder/utils/convertLongToBytes.js
@@ -2,6 +2,12 @@
 
 export const convertLongToBytes = (value, numberOfBytes) => {
   let longValue = value
+  const isValueNegative = value < 0
+  if (isValueNegative) {
+    // 2's complement
+    longValue = ~longValue
+    longValue += 1
+  }
   const byteArray = []
   for (let i = numberOfBytes; i > 0; i -= 1) {
     byteArray[numberOfBytes] = longValue & 255
