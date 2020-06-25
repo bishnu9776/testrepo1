d095abd19a21dcc4596a862c657428d581c7d93f
rosmirehman <rosmi@systeminsights.com>
2020-06-25 16:19:05 +0530

    DP-93: updated bikeInfo parsing

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,12 @@
 export const ACK_MSG_TAG = "ack"
-export const nonDataItemKeys = ["timestamp", "seq_num", "gpstime_utc", "global_seq", "bike_id", "data"]
+export const nonDataItemKeys = [
+  "timestamp",
+  "seq_num",
+  "gpstime_utc",
+  "global_seq",
+  "bike_id",
+  "data",
+  "sim_ccid",
+  "sim_cimi",
+  "vin"
+]
