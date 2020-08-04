71eacb6984ccee07f8a8b09bd484187515324783
rosmirehman <rosmi@systeminsights.com>
2020-08-04 13:01:11 +0530

    DP-166 - removed shouldDecode block and test from MCU parser

--- src/messageParser/channelParser/MCU.js
+++ src/messageParser/channelParser/MCU.js
@@ -2,17 +2,11 @@ import {parseMessage} from "./utils/parseMessage"
 import {getMCUDecoder} from "./channelDecoder/getMCUDecoder"
 
 export const parseMCU = () => {
-  const {env} = process
-  const shouldDecodeMessage = JSON.parse(env.VI_SHOULD_DECODE_MESSAGE || "false")
-  const decodeMCUMessage = shouldDecodeMessage ? getMCUDecoder() : null
+  const decodeMCUMessage = getMCUDecoder()
 
   return message => {
-    const {data, attributes} = message
-    let decodedMessage = data
-
-    if (shouldDecodeMessage) {
-      decodedMessage = decodeMCUMessage(message)
-    }
+    const {attributes} = message
+    const decodedMessage = decodeMCUMessage(message)
 
     return parseMessage(decodedMessage, attributes)
   }
