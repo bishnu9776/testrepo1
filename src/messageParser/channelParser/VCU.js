625b2dc5d3be41be0e20e1a42034e5f789925e50
rosmirehman <rosmi@systeminsights.com>
2020-08-04 13:03:49 +0530

    DP-166 - removed shouldDecode block and test from VCU parser

--- src/messageParser/channelParser/VCU.js
+++ src/messageParser/channelParser/VCU.js
@@ -2,17 +2,11 @@ import {parseMessage} from "./utils/parseMessage"
 import {getVCUDecoder} from "./channelDecoder/getVCUDecoder"
 
 export const parseVCU = () => {
-  const {env} = process
-  const shouldDecodeMessage = JSON.parse(env.VI_SHOULD_DECODE_MESSAGE || "false")
-  const decodeVCUMessage = shouldDecodeMessage ? getVCUDecoder() : null
+  const decodeVCUMessage = getVCUDecoder()
 
   return message => {
-    const {data, attributes} = message
-    let decodedMessage = data
-
-    if (shouldDecodeMessage) {
-      decodedMessage = decodeVCUMessage(message)
-    }
+    const {attributes} = message
+    const decodedMessage = decodeVCUMessage(message)
 
     return parseMessage(decodedMessage, attributes)
   }
