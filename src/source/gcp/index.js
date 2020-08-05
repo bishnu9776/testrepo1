3a9b4b2eae188766758ca77127ee7b35878108f7
rosmirehman <rosmi@systeminsights.com>
2020-08-05 17:00:56 +0530

    DP-146 - refactor

---
+++ src/source/gcp/index.js
@@ -0,0 +1,23 @@
+import {collectSubscriptionStats} from "../../metrics/subscriptionStats"
+import {getGCPStream} from "./gcpStream"
+
+export const getGCPSource = ({log, metricRegistry}) => {
+  const {env} = process
+  const subscriptionConfig = {
+    subscriptionName: env.VI_GCP_PUBSUB_BIKE_SUBSCRIPTION,
+    projectId: env.VI_GCP_PROJECT_ID,
+    credentialsPath: env.VI_GCP_SERVICE_ACCOUNT_CREDS_FILE_PATH
+  }
+
+  const {statsInterval} = metricRegistry
+  if (statsInterval) {
+    collectSubscriptionStats({metricRegistry, ...subscriptionConfig, statsInterval, log})
+  }
+  const {acknowledgeMessage, stream} = getGCPStream({
+    ...subscriptionConfig,
+    metricRegistry,
+    log
+  })
+
+  return {acknowledgeMessage, stream}
+}
