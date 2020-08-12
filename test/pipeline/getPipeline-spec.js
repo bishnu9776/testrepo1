a2db6328c9929d4dd9ee43f89d4928a1d16c1d1e
rosmirehman <rosmi@systeminsights.com>
2020-08-12 11:42:33 +0530

    DP-146 - added me2e test and skipped

--- test/pipeline/getPipeline-spec.js
+++ test/pipeline/getPipeline-spec.js
@@ -1,4 +1,5 @@
 import {from} from "rxjs"
+import fs from "fs"
 import * as kafkaProducer from "../../src/kafkaProducer"
 import {getPipeline} from "../../src/pipeline/getPipeline"
 import {getMockLog} from "../stubs/logger"
@@ -13,11 +14,9 @@ const {env} = process
 describe("Pipeline spec", () => {
   let probePath
   let appContext
-  let source
   const acknowledgeMessageSpy = sinon.spy()
 
   beforeEach(() => {
-    env.VI_ATHER_COLLECTOR_SOURCE_MODE = "gcp"
     env.VI_GCP_PUBSUB_DATA_COMPRESSION_FLAG = "false"
     env.VI_SHOULD_DEDUP_DATA = "true"
     setChannelDecoderConfigFileEnvs()
@@ -26,12 +25,6 @@ describe("Pipeline spec", () => {
       log: getMockLog()
     }
     probePath = `${process.cwd()}/test/fixtures/probe`
-    source = {
-      stream: from([
-        {message: getDecompressedGCPEvent("/test/fixtures/avro/CAN_MCU"), acknowledgeMessage: acknowledgeMessageSpy},
-        {message: "foobar", acknowledgeMessage: acknowledgeMessageSpy}
-      ])
-    }
 
     sinon.stub(kafkaProducer, "getKafkaSender").callsFake(() => {
       return stream => stream
@@ -43,7 +36,13 @@ describe("Pipeline spec", () => {
     clearStub()
   })
 
-  it("valid events flow through pipeline", done => {
+  it("valid events flow through pipeline from source gcp", done => {
+    const source = {
+      stream: from([
+        {message: getDecompressedGCPEvent("/test/fixtures/avro/CAN_MCU"), acknowledgeMessage: acknowledgeMessageSpy},
+        {message: "foobar", acknowledgeMessage: acknowledgeMessageSpy}
+      ])
+    }
     const output = []
     const observer = {
       next: message => {
@@ -52,6 +51,36 @@ describe("Pipeline spec", () => {
       complete: () => {
         expect(output.length).to.eql(22)
         expect(output.filter(e => e.channel === "can_mcu/v1_0_0").length).to.eql(20) // Two deduped CAN events
+        expect(output.filter(e => e.tag === ACK_MSG_TAG).length).to.eql(2) // two ack event, as we acknowledge invalid event also
+        expect(acknowledgeMessageSpy.callCount).to.eql(2)
+        done()
+      }
+    }
+
+    getPipeline({
+      source,
+      observer,
+      probePath,
+      kafkaProducer,
+      appContext
+    })
+  })
+
+  it.skip("valid events flow through pipeline from source kafka", done => {
+    const input = JSON.parse(fs.readFileSync(`${process.cwd()}/test/fixtures/avro/GEN_2`))
+    const source = {
+      stream: from([
+        {message: input, acknowledgeMessage: acknowledgeMessageSpy},
+        {message: "foobar", acknowledgeMessage: acknowledgeMessageSpy}
+      ])
+    }
+    const output = []
+    const observer = {
+      next: message => {
+        output.push(message)
+      },
+      complete: () => {
+        expect(output.length).to.eql(22)
         expect(output.filter(e => e.tag === ACK_MSG_TAG).length).to.eql(2) // two ack event, as we acknowledge invalid event also
         expect(acknowledgeMessageSpy.callCount).to.eql(2)
         done()
