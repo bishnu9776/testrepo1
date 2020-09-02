87a2741f8d81637bfa5023a6c2419eddd6a9d2df
rosmirehman <rosmi@systeminsights.com>
2020-09-02 16:40:55 +0530

    ATHER-196 - made drop events for device without mapping configurable

--- src/deviceModel/doesModelDeviceMappingPresent.js
+++
@@ -1,9 +0,0 @@
-export const doesModelDeviceMappingPresent = deviceModelMapping => event => {
-  if (event.tag === "ack") {
-    return true
-  }
-  if (deviceModelMapping[event.device_uuid]) {
-    return true
-  }
-  return false
-}
