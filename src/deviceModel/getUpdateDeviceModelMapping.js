2af44664c9a3594aa1f8247e336f087f9992894c
rosmirehman <rosmi@systeminsights.com>
2020-09-22 11:21:18 +0530

    ATHER-238 - small refactor

--- src/deviceModel/getUpdateDeviceModelMapping.js
+++ src/deviceModel/getUpdateDeviceModelMapping.js
@@ -4,7 +4,7 @@ import {makeAxiosRequest} from "../utils/makeAxiosRequest"
 import {errorFormatter} from "../utils/errorFormatter"
 import {isNilOrEmpty} from "../utils/isNilOrEmpty"
 
-export const putDeviceMapping = async ({appContext, device, model}) => {
+export const putDeviceMapping = async ({appContext, device, model, retryConfig}) => {
   const {apiConfig, log} = appContext
   const {plant, url} = apiConfig
   const endpoint = `${url}/${device}`
@@ -19,9 +19,7 @@ export const putDeviceMapping = async ({appContext, device, model}) => {
     },
     timeout: parseInt(process.env.VI_ATHER_COLLECTOR_REQUEST_TIMEOUT || "30000", 10)
   }
-  const isRetryable = is5xxError
 
-  const retryConfig = getRetryConfig(log, isRetryable)
   return retryableRequest({requestConfig, retryConfig, log, makeRequest: makeAxiosRequest})
 }
 
@@ -34,13 +32,15 @@ export const getUpdateDeviceModelMapping = appContext => {
   const {log} = appContext
   const deviceModelMappingMismatch = ({device, deviceModelMapping, model}) =>
     !deviceModelMapping[device] || deviceModelMapping[device] !== model
+  const isRetryable = is5xxError
+  const retryConfig = getRetryConfig(log, isRetryable)
 
   return async (deviceModelMapping, event) => {
     const device = event.device_uuid
     const valueKey = process.env.VI_VALUE_KEY || "value_event"
     const model = event && event[valueKey] ? getModel(event[valueKey]) : null
     if (!isNilOrEmpty(model) && deviceModelMappingMismatch({device, deviceModelMapping, model})) {
-      const {ok, response, error} = await putDeviceMapping({appContext, device, model})
+      const {ok, response, error} = await putDeviceMapping({appContext, device, model, retryConfig})
       if (ok && response) {
         // eslint-disable-next-line no-param-reassign
         deviceModelMapping[device] = model
