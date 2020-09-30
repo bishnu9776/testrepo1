868cdd3a8addfcef2d0669a773e120844f30327e
rsubramanyam2994 <subramanyam@systeminsights.com>
2020-09-30 19:09:48 +0530

    ATHER-244 Naming fixes

--- src/messageParser/index.js
+++ src/messageParser/index.js
@@ -6,7 +6,7 @@ import {getMergeProbeInfoFn} from "./mergeProbeInfo"
 import {dedupDataItems} from "./dedupDataItems"
 import {getAttributesFormatter} from "./formatAttributes"
 import {getChannelParser} from "./channelParser"
-import {getGCPMessageTags} from "../metrics/tags"
+import {getInputMessageTags} from "../metrics/tags"
 
 const {env} = process
 
@@ -50,7 +50,7 @@ export const getMessageParser = ({appContext, probe}) => {
     try {
       const attributes = formatAttributes(message.attributes)
       if (shouldDropChannel(attributes.channel) || shouldDropDevice(attributes.bike_id)) {
-        metricRegistry.updateStat("Counter", "num_input_messages_dropped", 1, getGCPMessageTags(message))
+        metricRegistry.updateStat("Counter", "num_input_messages_dropped", 1, getInputMessageTags(message))
         return endOfEvent
       }
 
