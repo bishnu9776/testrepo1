179b11816741b56d7ec8b5c96c9065ccdd1e2da4
siddhant <siddhant@systeminsights.com>
2020-11-25 14:44:08 +0530

    ATHER-297 Added gen based attribute parsing

--- src/source/kafka/kafkaStream.js
+++ src/source/kafka/kafkaStream.js
@@ -5,7 +5,22 @@ import {getInputMessageTags} from "../../metrics/tags"
 const isInvalidAttributes = attributes =>
   isNilOrEmpty(attributes[2]) && isNilOrEmpty(attributes[5]) && isNilOrEmpty(attributes[5])
 
-const getAttributes = (headers, metricRegistry) => {
+const getAttributesForGen1 = (headers, metricRegistry) => {
+  if (headers && headers[0].inputTopic) {
+    const attributesObj = headers[0].inputTopic.toString().split("/")
+    if (isInvalidAttributes(attributesObj)) {
+      metricRegistry.updateStat("Counter", "num_events_dropped", 1, "invalid_attributes")
+      throw new Error(`Device/channel not present, topic: ${headers[0].inputTopic}`)
+    }
+    return {
+      deviceId: attributesObj[3],
+      subFolder: `${attributesObj[5]}/${attributesObj[6]}`
+    }
+  }
+  throw new Error(`Invalid headers`)
+}
+
+const getAttributesForGen2 = (headers, metricRegistry) => {
   if (headers && headers[0].inputTopic) {
     const attributesObj = headers[0].inputTopic.toString().split(".")
     if (isInvalidAttributes(attributesObj)) {
@@ -20,28 +35,39 @@ const getAttributes = (headers, metricRegistry) => {
   throw new Error(`Invalid headers`)
 }
 
-const parseEvent = (appContext, event, resolve) => {
-  const {value, headers} = event
-  const {log, metricRegistry} = appContext
-  try {
-    const attributes = getAttributes(headers, metricRegistry)
-    if (event && isNilOrEmpty(value)) {
-      metricRegistry.updateStat("Counter", "num_events_dropped", 1, "parse_failure")
-      throw new Error(`Invalid Event value, event:${JSON.stringify(event)}`)
-    }
-    metricRegistry.updateStat("Counter", "num_messages_received", 1, getInputMessageTags({attributes}))
-    return {
-      message: {
-        data: event.value,
-        attributes
+const getAttributesForGen = gen => {
+  const genToGetAttributeFnMap = {
+    "gen-1": getAttributesForGen1,
+    "gen-2": getAttributesForGen2
+  }
+  return genToGetAttributeFnMap[gen]
+}
+
+const getParseEvent = appContext => {
+  const getAttributes = getAttributesForGen(process.env.VI_GEN)
+  return (event, resolve) => {
+    const {value, headers} = event
+    const {log, metricRegistry} = appContext
+    try {
+      const attributes = getAttributes(headers, metricRegistry)
+      if (event && isNilOrEmpty(value)) {
+        metricRegistry.updateStat("Counter", "num_events_dropped", 1, "parse_failure")
+        throw new Error(`Invalid Event value, event:${JSON.stringify(event)}`)
       }
+      metricRegistry.updateStat("Counter", "num_messages_received", 1, getInputMessageTags({attributes}))
+      return {
+        message: {
+          data: event.value,
+          attributes
+        }
+      }
+    } catch (e) {
+      log.warn(
+        {error: errorFormatter(e), ctx: {value: JSON.stringify(value), headers: JSON.stringify(headers)}},
+        `${e.message}`
+      )
+      resolve(event)
     }
-  } catch (e) {
-    log.warn(
-      {error: errorFormatter(e), ctx: {value: JSON.stringify(value), headers: JSON.stringify(headers)}},
-      `${e.message}`
-    )
-    resolve(event)
   }
 }
 
@@ -53,6 +79,8 @@ export const kafkaStream = (appContext, observer) => {
     }
   }
 
+  const parseEvent = getParseEvent(appContext)
+
   return batch => {
     return new Promise(resolve => {
       const acknowledgeMessage = event => {
@@ -60,7 +88,7 @@ export const kafkaStream = (appContext, observer) => {
       }
       const lastEvent = batch.pop()
       batch.forEach(event => {
-        const parsedEvent = parseEvent(appContext, event, resolve)
+        const parsedEvent = parseEvent(event, resolve)
         sendToObserver(parsedEvent)
       })
       const parsedLastEvent = parseEvent(appContext, lastEvent, resolve)
